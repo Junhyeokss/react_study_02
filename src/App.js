f93617eeb1613b54b599afab588fc0cf1c8dfa20
Junhyeokss <junhyeok0516@naver.com>
2023-02-28 12:40:24 +0900

    2023-02-28 shoppingmall

--- src/App.js
+++ src/App.js
@@ -3,28 +3,45 @@ import Content02 from "./components/Content02";
 import Content03 from "./components/Content03";
 import Footer from "./components/Footer";
 import Header from "./components//Header";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Detail from "./components/Detail";
 import { Data } from "./data/Data";
 import { useEffect, useState } from "react";
 
 const App = () => {
   const [itm, setItm] = useState([]);
-  const getItm = () => {
-    const itm = fetch(
+  const getItm = async () => {
+    const itm = await fetch(
       "https://desipossa.github.io/shop_cra/assets/data.json"
     ).then((r) => r.json());
+    console.log(itm);
+    setItm(itm);
   };
-  useEffect(() => {}, [itm]);
+
+  useEffect(() => {
+    getItm();
+  }, []);
+
   return (
     <>
       <Header />
+      <section>
+        {itm.slice(130, 150).map((it) => {
+          return (
+            <div>
+              <Link to={`/Detail/${it.id}`}>
+                <img src={it.image_link}></img>
+              </Link>
+            </div>
+          );
+        })}
+      </section>
       <Routes>
         <Route path="/" element={<Content01 />} />
         <Route path="/01" element={<Content01 />} />
         <Route path="/02" element={<Content02 />} />
         <Route path="/03" element={<Content03 />} />
-        <Route path="/detail/:id" element={<Detail Data={Data} />} />
+        <Route path="/Detail/:id" element={<Detail itm={itm} />} />
       </Routes>
       <Footer />
     </>
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,13 +1,17 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-const Detail = ({ Data }) => {
+const Detail = ({ itm }) => {
   const { id } = useParams();
-
+  const detailItm = itm.find((it) => it.id == id);
+  console.log(detailItm);
   //자룔를 가져와서 자료에 매칭되는 data를 여기에 뿌리고 싶다
+  // javascript에서 브라우즈 주소창 parameter 값과 일치하는 배열에 있는 값을 구하는 방법
   return (
     <>
-      <div>detail {Data[id].con}</div>
-      <div>detail {id}</div>
+      <div>detail{id}</div>
+      <div>
+        <img src={detailItm.image_link} />
+      </div>
     </>
   );
 };
